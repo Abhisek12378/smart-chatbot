b4a5349fb922c5a21086c6a69203bea6637d0f85
Abhisek Behera <abhisekbehera77744@gmail.com>
2024-04-08 01:29:30 +0530

    add delete files function

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import time
 from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,14 +8,14 @@ import os
 from handle_file import FileProcessor
 from datetime import datetime
 import traceback
+from fastapi import BackgroundTasks
 
-# Load environment variables from .env file (if any)
 load_dotenv()
 
 class Response(BaseModel):
     result: Union[str, None]
 
-# Consider adding your Heroku app URL to the origins list
+
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -34,12 +35,13 @@ app.add_middleware(
 # Define the directory to save uploaded files
 UPLOAD_DIR = "uploaded_files"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+@app.on_event("startup")
+async def startup_event():
+    BackgroundTasks().add_task(cleanup_files)
 
 @app.post("/predict", response_model=Response)
-async def predict(file: UploadFile = File(...), question: str = Form(...), timestamp: str = Form(...)) -> Response:
-    # Format and save the file
+async def predict(background_tasks: BackgroundTasks,file: UploadFile = File(...), question: str = Form(...), timestamp: str = Form(...)) -> Response:
     try:
-        # Save the file to the specified directory
         timestamp_dt = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
         formatted_str = timestamp_dt.strftime("%Y%m%d_%H%M%S")
         file_name = list(os.path.splitext(file.filename))[0] + "_" + formatted_str + list(os.path.splitext(file.filename))[1]
@@ -51,6 +53,7 @@ async def predict(file: UploadFile = File(...), question: str = Form(...), times
             pass
         file_process_obj = FileProcessor()
         query_result = str(file_process_obj.process_file(file_location, question))
+        background_tasks.add_task(cleanup_files)
         return {"result": query_result}
 
     except Exception as e:
@@ -58,3 +61,12 @@ async def predict(file: UploadFile = File(...), question: str = Form(...), times
         raise HTTPException(status_code=500, detail=str(e))
 
     return {"result": query_result}
+
+
+def cleanup_files():
+    current_time = time.time()
+    for filename in os.listdir(UPLOAD_DIR):
+        file_path = os.path.join(UPLOAD_DIR, filename)
+        file_mod_time = os.path.getmtime(file_path)
+        if current_time - file_mod_time > 3600:
+            os.remove(file_path)
